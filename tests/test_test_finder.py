5489f3246dcbc22f9de009c8efa70b2a116d9751
Pieter Swinkels <swinkels.pieter@yahoo.com>
2014-07-27 15:51:22 +0200

    Makes tests robust for the working directory of the test runner

--- tests/test_test_finder.py
+++ tests/test_test_finder.py
@@ -1,25 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import ast
+import os
 import unittest
 
 from skempy import test_finder
 
+def _get_file_path(file_name):
+    """Return the file path of the given file
+
+    The given file is specified relative to the current file.
+
+    """
+    return os.path.join(os.path.dirname(__file__), file_name)
+
 
 class TestTestFinder(unittest.TestCase):
 
-    def test_a(self):
-        test_path = test_finder.get_path("tests/source_code.py", 7)
+    def test_find_method_in_python_file_in_non_package_directory(self):
+        test_file_path = _get_file_path("source_code.py")
+        test_path = test_finder.get_path(test_file_path, 7)
         self.assertEqual("source_code.TestMe.test_a", test_path)
 
-    def test_b(self):
-        test_path = test_finder.get_path("tests/package/source_code.py", 7)
+    def test_find_method_in_python_file_in_package_directory(self):
+        test_file_path = _get_file_path("package/source_code.py")
+        test_path = test_finder.get_path(test_file_path, 7)
         self.assertEqual("package.source_code.TestMe.test_a", test_path)
 
 class TestLineFinder(unittest.TestCase):
 
     def test_find_method_by_cursor_position_in_body(self):
-        f = open("tests/source_code.py")
+        test_file_path = _get_file_path("source_code.py")
+        f = open(test_file_path)
         source_code = f.read()
         f.close()
 
@@ -30,7 +42,8 @@ class TestLineFinder(unittest.TestCase):
         self.assertEqual("TestMe.test_a", line_finder.path)
 
     def test_find_method_by_cursor_position_in_method_def(self):
-        f = open("tests/source_code.py")
+        test_file_path = _get_file_path("source_code.py")
+        f = open(test_file_path)
         source_code = f.read()
         f.close()
 
@@ -39,3 +52,27 @@ class TestLineFinder(unittest.TestCase):
         line_finder.visit(tree)
 
         self.assertEqual("TestMe.test_a", line_finder.path)
+
+    def test_find_method_by_cursor_position_in_class_def(self):
+        test_file_path = _get_file_path("source_code.py")
+        f = open(test_file_path)
+        source_code = f.read()
+        f.close()
+
+        tree = ast.parse(source_code)
+        line_finder = test_finder.LineFinder(line_no=4)
+        line_finder.visit(tree)
+
+        self.assertEqual("TestMe", line_finder.path)
+
+    def test_find_method_by_cursor_position_in_class_body_does_not_work(self):
+        test_file_path = _get_file_path("source_code.py")
+        f = open(test_file_path)
+        source_code = f.read()
+        f.close()
+
+        tree = ast.parse(source_code)
+        line_finder = test_finder.LineFinder(line_no=5)
+        line_finder.visit(tree)
+
+        self.assertNotEqual("TestMe", line_finder.path)
