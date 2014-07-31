8d2772037894b880ebc8ba7a96db76c4b64f4463
Pieter Swinkels <swinkels.pieter@yahoo.com>
2014-07-31 20:38:12 +0200

    Sets the version number in skempy/__init__.py instead of setup.py
    See the appropriate comment setup.py for the rational.

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 from setuptools import setup, find_packages
 
+# The following approach to retrieve the version number is inspired by this
+# comment:
+#
+#   https://github.com/zestsoftware/zest.releaser/issues/37#issuecomment-14496733
+#
+# With this approach, development installs always show the right version number
+# and do not require a reinstall (as the definition of the verdion umber in
+# this setup file would).
+
+version = 'no version defined'
+current_dir = os.path.dirname(__file__)
+with open(os.path.join(current_dir, "skempy", "__init__.py")) as f:
+    rx = re.compile('__version__ = (.*)')
+    for line in f:
+        m = rx.match(line)
+        if m:
+            version = m.group(1)
 
 with open('README.rst') as f:
     readme = f.read()
@@ -11,7 +31,7 @@ with open('LICENSE') as f:
 
 setup(
     name='skempy',
-    version='1.0.0.dev0',
+    version=version,
     description='Provides functionality to support Python development in Emacs',
     long_description=readme,
     author='P.C.J. Swinkels',
--- skempy/__init__.py
+++ skempy/__init__.py
@@ -1,3 +1,3 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution("skempy").version
+__version__ = '1.0.0.dev0'
