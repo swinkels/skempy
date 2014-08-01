4741af1bf1ef7dc49570e9502346c304c1b9c649
Pieter Swinkels <swinkels.pieter@yahoo.com>
2014-08-01 18:08:38 +0200

    Adds a version parameter to script skempy-find-test

--- skempy/find_path.py
+++ skempy/find_path.py
@@ -1,11 +1,20 @@
 from argparse import ArgumentParser
 
+import os
+import sys
+
 from skempy.test_finder import get_path
+
+import skempy
 
 
 def main():
-    parser = ArgumentParser(description="Retrieve the method in the given Python file and at the given line.")  # noqa
+    script_name = os.path.basename(sys.argv[0])
+    parser = ArgumentParser(prog=script_name,
+                            description="Retrieve the method in the given Python file and at the given line.")  # noqa
     parser.add_argument("file_path", help="Python file including path")
     parser.add_argument("line_no", help="line number", type=int)
+    version = "{} {}".format(script_name, skempy.__version__)
+    parser.add_argument("--version", action="version", version=version)
     args = parser.parse_args()
     print get_path(args.file_path, args.line_no)
