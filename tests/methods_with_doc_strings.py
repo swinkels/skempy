2c6a362fa7e10e4ad81f541338ec97d20e55b0c3
Pieter Swinkels <swinkels.pieter@yahoo.com>
2017-11-11 19:41:15 +0100

    Add missing file

---
+++ tests/methods_with_doc_strings.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestMe(unittest.TestCase):
+
+    def test_a(self):
+        """Test something."""
+        return
+
+    def test_b(self):
+        """Test another thing.
+
+        This is a multiline doc string.
+
+        """
+        return
